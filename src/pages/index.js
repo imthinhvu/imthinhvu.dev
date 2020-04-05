c67ba184a1fa1493486c11d89d2bbd7cdac89f79
Thinh Vu <im.thinhvu@gmail.com>
2020-04-05 14:35:27 -0700

    Change component names to utilize capitalization in file name.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-import Bio from "../components/bio";
-import Layout from "../components/layout";
+import Bio from "../components/Bio";
+import Layout from "../components/Layout";
 
 const SiteIndex = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata.title;
